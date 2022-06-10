ab46d14bb18f0ce884829d1533f47f1da79a32e7
Sharvan Saini <66919624+SharvanSaini@users.noreply.github.com>
2022-06-10 10:27:39 +0530

    Add files via upload

---
+++ ForumApplication/src/main/java/com/ForumApplication/JWTModels/Authority.java
@@ -0,0 +1,25 @@
+package com.ForumApplication.JWTModels;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class Authority implements GrantedAuthority {
+
+	
+	private String authority;
+	
+	
+	//constructor
+	public Authority(String authority) {
+		this.authority = authority;
+	}
+
+	@Override
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return this.authority;
+	}
+
+	
+	
+	
+}
